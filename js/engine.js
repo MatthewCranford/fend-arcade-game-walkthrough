5ace30ff321a05b7f8323c6dadfb604ed41fa6bb
Matthew Cranford <mcranfordmail@gmail.com>
2018-07-02 18:58:41 -0700

    Comment out dead code

--- js/engine.js
+++ js/engine.js
@@ -78,7 +78,7 @@ var Engine = (function(global) {
      * on the entities themselves within your app.js file).
      */
     function update(dt) {
-        updateEntities(dt);
+        // updateEntities(dt);
         // checkCollisions();
     }
 
@@ -138,7 +138,7 @@ var Engine = (function(global) {
             }
         }
 
-        renderEntities();
+        // renderEntities();
     }
 
     /* This function is called by the render function and is called on each game
