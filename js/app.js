21362909caf3c994a6ad266dad3439fa63ceafb4
Matthew Cranford <mcranfordmail@gmail.com>
2018-07-05 18:01:41 -0600

    Write some pseudo code

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    
+    // x pos
+    // y pos
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // If enemy is not passed boundary
+        // Move forward
+        // Increment x by speed * dt
+    // else
+        // Reset pos to start 
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,10 +34,37 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// Hero class
+    // Constructor
+        // Properties
+            // x pos
+            // y pos
+            // Sprite image
+        // Methods
+            // Update position
+                // Check collision here
+                    // Did player x and y collide with enemy?
+                // Check win here?
+                    // Did player x and y reach final tile?
+            // Render 
+                // Draw player sprite on current x and y coord position
+            // Handle keyboard input
+                  // Update player's x and y property according to input
+            // Reset Hero
+                // Set x and y to starting x and y
+
+
+              
+       
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+// New Hero object
+
+// Init allEnemies array 
+// For each enemy create and push new Enemy object into above array
 
 
 
